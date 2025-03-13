dcf0ed768b194ff761362b3f07eacb9671b2e277
babetCode <adrienbabet1@gmail.com>
2025-03-13 12:57:23 -0600

    - Custom user model (tutorial #3)

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email field must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=200, unique=True)
+    birthday = models.DateField(null=True, blank=True)
+    username = models.CharField(max_length=200, null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    # def __str__(self):
+    #     return self.username
