07b04eaa4ef4bd1da94b747327ae49b99b263134
babetCode <adrienbabet1@gmail.com>
2025-03-13 17:32:28 -0600

    - tutorial #4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,34 @@ import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Login from './components/Login'
 import Register from './components/Register'
-import { Routes, Route } from 'react-router-dom'
+import About from './components/About'
+import { Routes, Route, useLocation } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation()
+  const noNavbar = location.pathname === '/register' || location.pathname === '/'
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/home" element={<Home/>}/>
-      </Routes>
-      
+      {
+        noNavbar ?
+
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+
+        :
+
+        <Navbar
+          content={
+            <Routes>
+              <Route path="/home" element={<Home/>}/>
+              <Route path="/about" element={<About/>}/>
+            </Routes>
+          }
+        />
+      }
     </>
   )
 }
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,44 @@
+
+const About = () => {
+    return(
+        <div>
+            This is the about page
+        </div>
+    )
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const response = await axios.post("http://localhost:5000/auth/login", {
+//         email,
+//         password,
+//       });
+//       console.log(response.data);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h1>Login</h1>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="email"
+//           placeholder="Email"
+//           onChange={(e) => setEmail(e.target.value)}
+//         />
+//         <input
+//           type="password"
+//           placeholder="Password"
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+//         <button type="submit">Login</button>
+//       </form>
+//     </div>
+//   );
+}
+
+export default About
