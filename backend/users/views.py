775f34b212dc326061e84552ed54e87c05df6729
babetCode <adrienbabet1@gmail.com>
2025-03-13 21:45:38 -0600

    - tutorial #6 up to 12m45s

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import *
+from .models import *
+from rest_framework.response import Response
+from django.contrib.auth.models import get_user_model
+User = get_user_model()
 
 # Create your views here.
+
+class RegisterViewste(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
