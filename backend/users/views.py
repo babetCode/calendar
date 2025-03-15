b320b719c3ace5ccbe22205675641b9472b05c24
babetCode <adrienbabet1@gmail.com>
2025-03-15 17:10:49 -0600

    - tutorial #7

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,10 +3,31 @@ from rest_framework import viewsets, permissions
 from .serializers import *
 from .models import *
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from knox.models import AuthToken
+
 User = get_user_model()
 
-# Create your views here.
+class LoginViewset(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LoginSerializer
+    # queryset = User.objects.all()
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+
+            user = authenticate(request, email=email, password=password)
+
+            if user:
+                _, token = AuthToken.objects.create(user)
+                return Response({'user': self.serializer_class(user).data, 'token': token}, status=200)
+            else:
+                return Response({'error': 'Invalid credentials'}, status=401)
+        else:
+            return Response(serializer.errors, status=400)
 
 class RegisterViewset(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
